73711581579f934ce1567df7fde4dd64847c1b62
Mark Dujali <mddujali@gmail.com>
2026-06-14 01:49:27 +0800

    chore: publish lang folder

--- rector.php
+++ rector.php
@@ -13,6 +13,7 @@ return static function (RectorConfig $config): void {
         __DIR__ . '/bootstrap/app.php',
         __DIR__ . '/bootstrap/providers.php',
         __DIR__ . '/config',
+        __DIR__ . '/lang',
         __DIR__ . '/resources',
         __DIR__ . '/routes',
         __DIR__ . '/tests',
